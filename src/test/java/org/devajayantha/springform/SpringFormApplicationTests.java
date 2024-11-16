51c5e258ea0b32faf2cd59dff9882eb0c6bf08e3
Devajayantha <devajayantha888@gmail.com>
2024-11-16 20:46:38 +0800

    first commit

---
+++ src/test/java/org/devajayantha/springform/SpringFormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devajayantha.springform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
